3b2fcd93070a4ee86e344aecbd4026e5ffe83a97
Maarten Lankhorst <maarten.lankhorst@canonical.com>
2012-10-25 19:46:22 +0200

    dsound: create a primary_pwfx separately from pwfx
    I promised I would never touch it, but then I wanted to play skyrim
    
    V2: Limit channels to 2 in primary if not in writeprimary mode

--- dlls/dsound/dsound_private.h
+++ dlls/dsound/dsound_private.h
@@ -72,7 +72,7 @@ struct DirectSoundDevice
     GUID                        guid;
     DSCAPS                      drvcaps;
     DWORD                       priolevel;
-    PWAVEFORMATEX               pwfx;
+    PWAVEFORMATEX               pwfx, primary_pwfx;
     UINT                        timerID, playing_offs_bytes, in_mmdev_bytes, prebuf, helfrags;
     DWORD                       fraglen;
     LPBYTE                      buffer;
@@ -211,6 +211,7 @@ HRESULT DSOUND_PrimaryStop(DirectSoundDevice *device) DECLSPEC_HIDDEN;
 HRESULT DSOUND_PrimaryGetPosition(DirectSoundDevice *device, LPDWORD playpos, LPDWORD writepos) DECLSPEC_HIDDEN;
 LPWAVEFORMATEX DSOUND_CopyFormat(LPCWAVEFORMATEX wfex) DECLSPEC_HIDDEN;
 HRESULT DSOUND_ReopenDevice(DirectSoundDevice *device, BOOL forcewave) DECLSPEC_HIDDEN;
+HRESULT DSOUND_PrimaryOpen(DirectSoundDevice *device) DECLSPEC_HIDDEN;
 HRESULT primarybuffer_create(DirectSoundDevice *device, IDirectSoundBufferImpl **ppdsb,
     const DSBUFFERDESC *dsbd) DECLSPEC_HIDDEN;
 void primarybuffer_destroy(IDirectSoundBufferImpl *This) DECLSPEC_HIDDEN;
