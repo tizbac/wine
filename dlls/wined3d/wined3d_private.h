21f4e461dcd80ca5009a2444adc1a63482442a3b
Stefan Dösinger <stefan@codeweavers.com>
2013-10-03 19:23:24 +0200

    wined3d: Remove software cursor support.
    This has been broken since quite some time and only serves to crash fglrx by acquiring a context
    in the main thread.
    
    FIXME: Make sure wined3d_device_show_cursor returns the correct value if a software cursor is set.

--- dlls/wined3d/wined3d_private.h
+++ dlls/wined3d/wined3d_private.h
@@ -1967,7 +1967,6 @@ struct wined3d_device
     UINT                    xScreenSpace;
     UINT                    yScreenSpace;
     UINT                    cursorWidth, cursorHeight;
-    struct wined3d_texture *cursor_texture;
     HCURSOR                 hardwareCursor;
 
     /* The Wine logo texture */
